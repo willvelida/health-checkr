d81c24e55d976758266ecd336674d207e6b1fe50
Will Velida <willvelida@microsoft.com>
2022-12-30 12:39:16 +1300

    Adding id property for sleep envelope

--- HealthCheckr.Sleep/src/HealthCheckr.Sleep/HealthCheckr.Sleep.Common/Envelopes/SleepEnvelope.cs
+++ HealthCheckr.Sleep/src/HealthCheckr.Sleep/HealthCheckr.Sleep.Common/Envelopes/SleepEnvelope.cs
@@ -1,9 +1,12 @@
 ﻿using HealthCheckr.Sleep.Common.FitbitResponses;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace HealthCheckr.Sleep.Common.Envelopes
 {
     public class SleepEnvelope
     {
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
         public SleepResponseObject Sleep { get; set; }
         public string Date { get; set; }
