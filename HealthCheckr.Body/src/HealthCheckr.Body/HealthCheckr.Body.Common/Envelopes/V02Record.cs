e3d0b02b6b4ce54324bb73e126e34f959d507e6f
willvelida <willvelida@hotmail.co.uk>
2023-01-08 18:04:13 +1300

    Annotating values with keys

--- HealthCheckr.Body/src/HealthCheckr.Body/HealthCheckr.Body.Common/Envelopes/V02Record.cs
+++ HealthCheckr.Body/src/HealthCheckr.Body/HealthCheckr.Body.Common/Envelopes/V02Record.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HealthCheckr.Body.Common.Envelopes
 {
     public class V02Record
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string V02Max { get; set; }
--- HealthCheckr.Body/src/HealthCheckr.Body/HealthCheckr.Body.Common/Envelopes/WeightRecord.cs
+++ HealthCheckr.Body/src/HealthCheckr.Body/HealthCheckr.Body.Common/Envelopes/WeightRecord.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HealthCheckr.Body.Common.Envelopes
 {
     public class WeightRecord
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public double BmiValue { get; set; }
