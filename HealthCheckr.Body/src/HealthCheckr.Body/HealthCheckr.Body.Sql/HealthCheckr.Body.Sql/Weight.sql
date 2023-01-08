33454b5f73e4aa64d04279dd21ec7f546fb59473
willvelida <willvelida@hotmail.co.uk>
2023-01-08 14:28:46 +1300

    Adding SQL tables for Body service and updating workflow

---
+++ HealthCheckr.Body/src/HealthCheckr.Body/HealthCheckr.Body.Sql/HealthCheckr.Body.Sql/Weight.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[Weight]
+(
+  [Id] INT NOT NULL PRIMARY KEY,
+  [BmiValue] DECIMAL(5,2) NOT NULL,
+  [Date] DATE NOT NULL,
+  [FatPercentage] DECIMAL(5,2) NOT NULL,
+  [Source] VARCHAR(100) NOT NULL,
+  [Time] TIME NOT NULL,
+  [WeightInKG] DECIMAL(5, 2) NOT NULL
+)
