57369ff8f4c77e78759cf02ac0bac4328da74cf2
willvelida <willvelida@hotmail.co.uk>
2023-01-09 20:21:07 +1300

    Adding Sleep tables in Sql

---
+++ HealthCheckr.Sleep/src/HealthCheckr.Sleep/HealthCheckr.Sleep.Sql/Sleep.sql
@@ -0,0 +1,49 @@
+CREATE TABLE [dbo].[SleepSummary] (
+    [Id]                 INT  IDENTITY NOT NULL PRIMARY KEY CLUSTERED ([Id] ASC),
+    [TotalMinutesAsleep] INT  NOT NULL,
+    [TotalSleepRecords]  INT  NOT NULL,
+    [TotalTimeInBed]     INT  NOT NULL,
+    [DeepSleep]          INT  NOT NULL,
+    [LightSleep]         INT  NOT NULL,
+    [REMSleep]           INT  NOT NULL,
+    [AwakeMinutes]       INT  NOT NULL,
+    [Date]               DATE NOT NULL,
+);
+GO;
+
+CREATE TABLE [dbo].[Sp02] (
+  [Id] INT IDENTITY NOT NULL PRIMARY KEY,
+  [DATE] DATE NOT NULL,
+  [AvgValue] DECIMAL(5,2) NOT NULL,
+  [MinValue] DECIMAL(5,2) NOT NULL,
+  [MaxValue] DECIMAL(5,2) NOT NULL
+)
+GO;
+
+CREATE TABLE [dbo].[BreathingRate] (
+  [Id] INT IDENTITY NOT NULL PRIMARY KEY,
+  [Date] DATE NOT NULL,
+  [BreathingRate] DECIMAL(5,2) NOT NULL
+)
+GO;
+
+CREATE TABLE [dbo].[Sleep]
+(
+  [Id] INT IDENTITY NOT NULL PRIMARY KEY,
+  [Date] DATE NOT NULL,
+  [Efficiency] INT NOT NULL,
+  [Duration] INT NOT NULL,
+  [StartTime] DATETIME NOT NULL,
+  [EndTime] DATETIME NOT NULL,
+  [MinutesAfterWakeup] INT NOT NULL,
+  [MinutesAsleep] INT NOT NULL,
+  [MinutesAwake] INT NOT NULL,
+  [MinutesToFallAsleep] INT NOT NULL,
+  [RestlessCount] INT NOT NULL,
+  [RestlessDuration] INT NOT NULL,
+  [TimeInBed] INT NOT NULL,
+  CONSTRAINT [SleepSummaryId] FOREIGN KEY ([Id]) REFERENCES [dbo].[SleepSummary] ([Id]),
+  CONSTRAINT [Sp02Id] FOREIGN KEY ([Id]) REFERENCES [dbo].[Sp02] ([Id]),
+  CONSTRAINT [BreathingRateId] FOREIGN KEY ([Id]) REFERENCES [dbo].[BreathingRate] ([Id])
+)
+GO;
